eabc543cfa4b8390013f173582a3e8559b39e37d
Naveenjnk127 <19tuic018@skct.edu.in>
2023-09-16 15:52:58 +0530

    login form base version

---
+++ script.js
@@ -0,0 +1,4 @@
+const email = document.querySelector("#email");
+const phoneNumber = document.querySelector("#phone-number");
+const password = document.querySelector("#password");
+const confirmPassword = document.querySelector("#confirm-password");
